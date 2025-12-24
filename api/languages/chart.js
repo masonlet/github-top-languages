afe68a4901379002135e637700a2513ede1542c4
Masonlet <masonletoile@hotmail.com>
2025-12-24 00:24:07 -0500

    Refactor chart generation files
    - Move donut logic from chart.js to charts/donut.js
    - Chart.js is a simple route for chart type selection
    - Preparation for additional chart types

--- api/languages/chart.js
+++ api/languages/chart.js
@@ -1,51 +1,5 @@
 import { processLanguageData } from './data.js';
-import { createDonutSegments } from './geometry/donut.js';
-import { createLegend } from './legend.js';
-import { 
-  DONUT_GEOMETRY, 
-  LEGEND_SHIFT_THRESHOLD,
-  LEGEND_STYLES
-} from './constants.js';
-
-function calculateDonutCenter(width, isShifted) {
-  const legendWidth = isShifted
-    ? LEGEND_STYLES.COLUMN_WIDTH * 2
-    : LEGEND_STYLES.WIDTH;
-
-  const availableSpace = width - legendWidth - DONUT_GEOMETRY.MARGIN_RIGHT;
-
-  return availableSpace / 2;
-}
-
-function calculateLegendStartX(chartCenterX, donutRadius, isShifted) {
-  const legendWidth = isShifted
-    ? LEGEND_STYLES.COLUMN_WIDTH * 2
-    : LEGEND_STYLES.WIDTH;
-
-  return chartCenterX + donutRadius + DONUT_GEOMETRY.MARGIN_RIGHT;
-}
-
-function generateDonutChart(normalizedLanguages, selectedTheme, width) {
-  const isShifted = normalizedLanguages.length > LEGEND_SHIFT_THRESHOLD;
-  const chartX = calculateDonutCenter(width, isShifted);
-  const legendStartX = calculateLegendStartX(chartX, DONUT_GEOMETRY.OUTER_RADIUS, isShifted);
-
-  const segments = createDonutSegments(
-    normalizedLanguages,
-    chartX,
-    DONUT_GEOMETRY,
-    selectedTheme.colours
-  );
-
-  const legend = createLegend(
-    normalizedLanguages,
-    isShifted,
-    selectedTheme,
-    legendStartX
-  )
-
-  return { segments, legend };
-}
+import { generateDonutChart } from './charts/donut.js';
 
 export function generateChartData(languageBytes, langCount, selectedTheme, chartType, width) {
   const normalizedLanguages = processLanguageData(languageBytes, langCount);
---
+++ api/languages/charts/donut.js
@@ -0,0 +1,48 @@
+import { createDonutSegments } from '../geometry/donut.js';
+import { createLegend } from '../legend.js';
+import { 
+  DONUT_GEOMETRY, 
+  LEGEND_SHIFT_THRESHOLD,
+  LEGEND_STYLES
+} from '../constants.js';
+
+function calculateDonutCenter(width, isShifted) {
+  const legendWidth = isShifted
+    ? LEGEND_STYLES.COLUMN_WIDTH * 2
+    : LEGEND_STYLES.WIDTH;
+
+  const availableSpace = width - legendWidth - DONUT_GEOMETRY.MARGIN_RIGHT;
+
+  return availableSpace / 2;
+}
+
+function calculateLegendStartX(chartCenterX, donutRadius, isShifted) {
+  const legendWidth = isShifted
+    ? LEGEND_STYLES.COLUMN_WIDTH * 2
+    : LEGEND_STYLES.WIDTH;
+
+  return chartCenterX + donutRadius + DONUT_GEOMETRY.MARGIN_RIGHT;
+}
+
+export function generateDonutChart(normalizedLanguages, selectedTheme, width) {
+  const isShifted = normalizedLanguages.length > LEGEND_SHIFT_THRESHOLD;
+  const chartX = calculateDonutCenter(width, isShifted);
+  const legendStartX = calculateLegendStartX(chartX, DONUT_GEOMETRY.OUTER_RADIUS, isShifted);
+
+  const segments = createDonutSegments(
+    normalizedLanguages,
+    chartX,
+    DONUT_GEOMETRY,
+    selectedTheme.colours
+  );
+
+  const legend = createLegend(
+    normalizedLanguages,
+    isShifted,
+    selectedTheme,
+    legendStartX
+  )
+
+  return { segments, legend };
+}
+
